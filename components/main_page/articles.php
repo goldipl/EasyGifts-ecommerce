57c91419a493fa829ac67657317b8020860a3f3a
goldipl <mgodlewski92@gmail.com>
2025-03-05 20:23:48 +0100

    fill and styling articles banners

--- components/main_page/articles.php
+++ components/main_page/articles.php
@@ -1,33 +1,15 @@
 <section id="articles">
-    <div class="articles_container container d-flex justify-content-between">
-        <div class="articles_container__slot w-100 macma-catalog">
-            <div class="articles_container__slot--title d-flex justify-content-between align-items-center">
-                <div class="article-title">
-                    <h2>Katalog Macma</h2>
-                </div>
-            </div>
-            <div class="articles_container__slot--img">
-                <img src="./assets/img/main_page/articles/macma_catalog.jpg" alt="article img" width="765" height="208">
-            </div>
-            <div class="articles_container__slot--text">
-                <p>
-                Lorem ipsum dolor sit amet consectetur. Nisl ipsum morbi tincidunt vivamus lectus aliquam id diam quisque. Praesent massa rutrum non nunc nisi scelerisque nibh scelerisque vulputate. Sed sagittis aliquet iaculis praesent urna praesent. Enim viverra nisl leo sit pretium non elementum dui nec. Elit hendrerit risus lorem sed imperdiet mauris est tellus. Pellentesque turpis accumsan aliquam eu ultrices massa nunc vel sodales. Natoque at tincidunt scelerisque nisl et elit. Quis sociis id placerat amet elementum aliquet non fringilla. <a href="#">Zobacz więcej</a>
-                </p>
+    <div class="articles_container container">
+        <div class="articles_container__slot">
+            <div class="article-slot-img">
+                <img class="article-img" src="./assets/img/main_page/articles/article01.jpg" alt="article" width="776" height="477">
+                <a class="white-purple-btn" href="#our_products_container">Sprawdź<img src="./assets/icons/common/buttons/button_color_arrow.svg" alt="arrow icon" width="18" height="18"></a>
             </div>
         </div>
-        <div class="articles_container__slot w-100 macma-collection">
-            <div class="articles_container__slot--title d-flex justify-content-between align-items-center">
-                <div class="article-title">
-                    <h2>M-collection</h2>
-                </div>
-            </div>
-            <div class="articles_container__slot--img">
-                <img src="./assets/img/main_page/articles/m-collection.jpg" alt="article img" width="765" height="208">
-            </div>
-            <div class="articles_container__slot--text">
-                <p>
-                Lorem ipsum dolor sit amet consectetur. Nisl ipsum morbi tincidunt vivamus lectus aliquam id diam quisque. Praesent massa rutrum non nunc nisi scelerisque nibh scelerisque vulputate. Sed sagittis aliquet iaculis praesent urna praesent. Enim viverra nisl leo sit pretium non elementum dui nec. Elit hendrerit risus lorem sed imperdiet mauris est tellus. Pellentesque turpis accumsan aliquam eu ultrices massa nunc vel sodales. Natoque at tincidunt scelerisque nisl et elit. Quis sociis id placerat amet elementum aliquet non fringilla. <a href="#">Zobacz więcej</a>
-                </p>
+        <div class="articles_container__slot">
+            <div class="article-slot-img">
+                <img class="article-img" src="./assets/img/main_page/articles/article02.jpg" alt="article" width="776" height="477">
+                <a class="white-purple-btn" href="#our_products_container">Sprawdź<img src="./assets/icons/common/buttons/button_color_arrow.svg" alt="arrow icon" width="18" height="18"></a>
             </div>
         </div>
     </div>
