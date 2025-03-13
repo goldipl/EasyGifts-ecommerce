12f05d970d1e596939560d93c15e436150a9f749
goldipl <mgodlewski92@gmail.com>
2025-03-13 17:59:06 +0100

    fill and styling blog and news section

--- components/main_page/blog.php
+++ components/main_page/blog.php
@@ -1,149 +1,167 @@
 <section id="blog">
-    <div class="container d-flex flex-row align-items-center justify-content-center">
-        <div class="d-flex flex-column py-4 mp-titles align-items-center justify-content-center">
-            <div class="d-flex flex-row mp-superscription">
-                <h3>Co u nas słychać?</h3>
-            </div>
-            <div class="d-flex flex-row mp-title">
-                <h2>Najnowsze wpisy na blogu</h2>
-            </div>
-        </div>
+  <div class="container d-flex flex-row align-items-center justify-content-center">
+    <div class="d-flex flex-column py-4 mp-titles align-items-center justify-content-center">
+      <div class="d-flex flex-row mp-superscription">
+        <h3>Co u nas słychać?</h3>
+      </div>
+      <div class="d-flex flex-row mp-title">
+        <h2>Najnowsze wpisy na blogu</h2>
+      </div>
+      <div class="d-flex flex-row mp-description">
+        <span>Przestrzeń pełna praktycznych porad, nowinek technologicznych i rozwiązań biznesowych.</span>
+      </div>
     </div>
-
-    <div id="blog_swiper" class="container position-relative">
-        <div class="blog_swiper">
-            <!-- Additional required wrapper -->
-            <div class="swiper-wrapper">
-                <!-- Slide 01 -->
-                <div class="swiper-slide">
-                    <div class="article_swiper_box">
-                        <div class="article_swiper_box__image">
-                            <img src="./assets/img/main_page/newest_articles/newest_articles_01.jpg" alt="news img" width="498" height="408">
-                        </div>
-                        <div class="article_swiper_box__text">
-                            <div class="article_swiper_box__text--date">
-                                <p>Poniedziałek, 12 czerwca 2023</p>
-                            </div>
-                            <div class="article_swiper_box__text--title">
-                                <h3>
-                                    <a href="#">Letnie orzeźwienie w reklamie</a>
-                                </h3>
-                            </div>
-                            <div class="article_swiper_box__text--see-more">
-                                <a href="#" rel="nofollow">Zobacz więcej</a>
-                            </div>
-                        </div>  
-                    </div>
-                </div>
-                <!-- Slide 02 -->
-                <div class="swiper-slide">
-                    <div class="article_swiper_box">
-                        <div class="article_swiper_box__image">
-                            <img src="./assets/img/main_page/newest_articles/newest_articles_02.jpg" alt="news img" width="498" height="408">
-                        </div>
-                        <div class="article_swiper_box__text">
-                            <div class="article_swiper_box__text--date">
-                                <p>Poniedziałek, 12 czerwca 2023</p>
-                            </div>
-                            <div class="article_swiper_box__text--title">
-                                <h3>
-                                    <a href="#">High season w branży gadżetów reklamowych. Jak się do niego przygotować?</a>
-                                </h3>
-                            </div>
-                            <div class="article_swiper_box__text--see-more">
-                                <a href="#" rel="nofollow">Zobacz więcej</a>
-                            </div>
-                        </div>  
-                    </div>
-                </div>
-                <!-- Slide 03 -->
-                <div class="swiper-slide">
-                    <div class="article_swiper_box">
-                        <div class="article_swiper_box__image">
-                            <img src="./assets/img/main_page/newest_articles/newest_articles_03.jpg" alt="news img" width="498" height="408">
-                        </div>
-                        <div class="article_swiper_box__text">
-                            <div class="article_swiper_box__text--date">
-                                <p>Poniedziałek, 12 czerwca 2023</p>
-                            </div>
-                            <div class="article_swiper_box__text--title">
-                                <h3>
-                                    <a href="#">Jak wybrać najlepszą hurtownię gadżetów reklamowych?</a>
-                                </h3>
-                            </div>
-                            <div class="article_swiper_box__text--see-more">
-                                <a href="#" rel="nofollow">Zobacz więcej</a>
-                            </div>
-                        </div>  
-                    </div>
-                </div>
-                <!-- Slide 04 -->
-                <div class="swiper-slide">
-                    <div class="article_swiper_box">
-                        <div class="article_swiper_box__image">
-                            <img src="./assets/img/main_page/newest_articles/newest_articles_02.jpg" alt="news img" width="498" height="408">
-                        </div>
-                        <div class="article_swiper_box__text">
-                            <div class="article_swiper_box__text--date">
-                                <p>Poniedziałek, 12 czerwca 2023</p>
-                            </div>
-                            <div class="article_swiper_box__text--title">
-                                <h3>
-                                    <a href="#">High season w branży gadżetów reklamowych. Jak się do niego przygotować?</a>
-                                </h3>
-                            </div>
-                            <div class="article_swiper_box__text--see-more">
-                                <a href="#" rel="nofollow">Zobacz więcej</a>
-                            </div>
-                        </div>  
-                    </div>
-                </div>
-                <!-- Slide 05 -->
-                <div class="swiper-slide">
-                    <div class="article_swiper_box">
-                        <div class="article_swiper_box__image">
-                            <img src="./assets/img/main_page/newest_articles/newest_articles_02.jpg" alt="news img" width="498" height="408">
-                        </div>
-                        <div class="article_swiper_box__text">
-                            <div class="article_swiper_box__text--date">
-                                <p>Poniedziałek, 12 czerwca 2023</p>
-                            </div>
-                            <div class="article_swiper_box__text--title">
-                                <h3>
-                                    <a href="#">High season w branży gadżetów reklamowych. Jak się do niego przygotować?</a>
-                                </h3>
-                            </div>
-                            <div class="article_swiper_box__text--see-more">
-                                <a href="#" rel="nofollow">Zobacz więcej</a>
-                            </div>
-                        </div>  
-                    </div>
-                </div>
-                <!-- Slide 06 -->
-                <div class="swiper-slide">
-                    <div class="article_swiper_box">
-                        <div class="article_swiper_box__image">
-                            <img src="./assets/img/main_page/newest_articles/newest_articles_02.jpg" alt="news img" width="498" height="408">
-                        </div>
-                        <div class="article_swiper_box__text">
-                            <div class="article_swiper_box__text--date">
-                                <p>Poniedziałek, 12 czerwca 2023</p>
-                            </div>
-                            <div class="article_swiper_box__text--title">
-                                <h3>
-                                    <a href="#">High season w branży gadżetów reklamowych. Jak się do niego przygotować?</a>
-                                </h3>
-                            </div>
-                            <div class="article_swiper_box__text--see-more">
-                                <a href="#" rel="nofollow">Zobacz więcej</a>
-                            </div>
-                        </div>  
-                    </div>
-                </div>
+  </div>
+  <div id="blog_swiper" class="container position-relative">
+    <div class="blog_swiper">
+      <!-- Additional required wrapper -->
+      <div class="swiper-wrapper">
+        <!-- Slide 01 -->
+        <div class="swiper-slide">
+          <div class="article_swiper_box">
+            <div class="article_swiper_box__image">
+              <img src="./assets/img/main_page/blog/blog01.png" alt="news img" width="310" height="251">
+              <div class="article_swiper_box__image--date">
+                <img src="./assets/icons/blog/calendar-icon.svg" alt="kalendarz" width="13" height="13">
+                <p>27 Grudzień, 2024</p>
+              </div>
             </div>
-            <!-- If we need navigation buttons -->
-            <div class="swiper-button-prev"></div>
-            <div class="swiper-button-next"></div>
+            <div class="article_swiper_box__text">
+              <div class="article_swiper_box__text--title">
+                <h3>
+                  <a href="#">Jakie gadżety na 2025 rok? Najnowsze trendy i topowe propozycje!</a>
+                </h3>
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit.Vivamus dictum turpis id diam accumsan, ut rhoncus purusviverra. Vivamus dictum turpis id diam accumsan, ut rhoncus purusviverra. </p>
+              </div>
+              <div class="article_swiper_box__text--see-more">
+                <a href="#" rel="nofollow">Czytaj więcej</a>
+              </div>
+            </div>
+          </div>
         </div>
+        <!-- Slide 02 -->
+        <div class="swiper-slide">
+          <div class="article_swiper_box">
+            <div class="article_swiper_box__image">
+              <img src="./assets/img/main_page/blog/blog01.png" alt="news img" width="310" height="251">
+              <div class="article_swiper_box__image--date">
+                <img src="./assets/icons/blog/calendar-icon.svg" alt="kalendarz" width="13" height="13">
+                <p>27 Grudzień, 2024</p>
+              </div>
+            </div>
+            <div class="article_swiper_box__text">
+              <div class="article_swiper_box__text--title">
+                <h3>
+                  <a href="#">5 błędów przy wyborze gadżetów reklamowych</a>
+                </h3>
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit.Vivamus dictum turpis id diam accumsan, ut rhoncus purusviverra. Vivamus dictum turpis id diam accumsan, ut rhoncus purusviverra. </p>
+              </div>
+              <div class="article_swiper_box__text--see-more">
+                <a href="#" rel="nofollow">Czytaj więcej</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- Slide 03 -->
+        <div class="swiper-slide">
+          <div class="article_swiper_box">
+            <div class="article_swiper_box__image">
+              <img src="./assets/img/main_page/blog/blog02.png" alt="news img" width="310" height="251">
+              <div class="article_swiper_box__image--date">
+                <img src="./assets/icons/blog/calendar-icon.svg" alt="kalendarz" width="13" height="13">
+                <p>27 Grudzień, 2024</p>
+              </div>
+            </div>
+            <div class="article_swiper_box__text">
+              <div class="article_swiper_box__text--title">
+                <h3>
+                  <a href="#">Czas na odblaski!</a>
+                </h3>
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit.Vivamus dictum turpis id diam accumsan, ut rhoncus purusviverra. Vivamus dictum turpis id diam accumsan, ut rhoncus purusviverra. </p>
+              </div>
+              <div class="article_swiper_box__text--see-more">
+                <a href="#" rel="nofollow">Czytaj więcej</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- Slide 04 -->
+        <div class="swiper-slide">
+          <div class="article_swiper_box">
+            <div class="article_swiper_box__image">
+              <img src="./assets/img/main_page/blog/blog03.png" alt="news img" width="310" height="251">
+              <div class="article_swiper_box__image--date">
+                <img src="./assets/icons/blog/calendar-icon.svg" alt="kalendarz" width="13" height="13">
+                <p>27 Grudzień, 2024</p>
+              </div>
+            </div>
+            <div class="article_swiper_box__text">
+              <div class="article_swiper_box__text--title">
+                <h3>
+                  <a href="#">Molu — Rewolucja w świecie gadżetów reklamowych</a>
+                </h3>
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit.Vivamus dictum turpis id diam accumsan, ut rhoncus purusviverra. Vivamus dictum turpis id diam accumsan, ut rhoncus purusviverra. </p>
+              </div>
+              <div class="article_swiper_box__text--see-more">
+                <a href="#" rel="nofollow">Czytaj więcej</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- Slide 05 -->
+        <div class="swiper-slide">
+          <div class="article_swiper_box">
+            <div class="article_swiper_box__image">
+              <img src="./assets/img/main_page/blog/blog04.png" alt="news img" width="310" height="251">
+              <div class="article_swiper_box__image--date">
+                <img src="./assets/icons/blog/calendar-icon.svg" alt="kalendarz" width="13" height="13">
+                <p>27 Grudzień, 2024</p>
+              </div>
+            </div>
+            <div class="article_swiper_box__text">
+              <div class="article_swiper_box__text--title">
+                <h3>
+                  <a href="#">Jakie gadżety na 2025 rok? Najnowsze trendy i topowe propozycje!</a>
+                </h3>
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit.Vivamus dictum turpis id diam accumsan, ut rhoncus purusviverra. Vivamus dictum turpis id diam accumsan, ut rhoncus purusviverra. </p>
+              </div>
+              <div class="article_swiper_box__text--see-more">
+                <a href="#" rel="nofollow">Czytaj więcej</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- Slide 06 -->
+        <div class="swiper-slide">
+          <div class="article_swiper_box">
+            <div class="article_swiper_box__image">
+              <img src="./assets/img/main_page/blog/blog01.png" alt="news img" width="310" height="251">
+              <div class="article_swiper_box__image--date">
+                <img src="./assets/icons/blog/calendar-icon.svg" alt="kalendarz" width="13" height="13">
+                <p>27 Grudzień, 2024</p>
+              </div>
+            </div>
+            <div class="article_swiper_box__text">
+              <div class="article_swiper_box__text--title">
+                <h3>
+                  <a href="#">Jakie gadżety na 2025 rok? Najnowsze trendy i topowe propozycje!</a>
+                </h3>
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit.Vivamus dictum turpis id diam accumsan, ut rhoncus purusviverra. Vivamus dictum turpis id diam accumsan, ut rhoncus purusviverra. </p>
+              </div>
+              <div class="article_swiper_box__text--see-more">
+                <a href="#" rel="nofollow">Czytaj więcej</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- If we need navigation buttons -->
+      <div class="swiper-button-prev"></div>
+      <div class="swiper-button-next"></div>
     </div>
+  </div>
+  <div class="main-check-btn-box">
+    <a href="" class="white-pink-btn">Sprawdź wszystkie <img src="./assets/icons/common/buttons/button_pink_arrow.svg" alt="arrow icon" width="18" height="18">
+    </a>
+  </div>
 </section>
