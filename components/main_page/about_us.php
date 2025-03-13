44f2a792da2732a1275832c6ab6fdc25d0792ed1
goldipl <mgodlewski92@gmail.com>
2025-03-13 18:41:04 +0100

    fill & styling abouts us section

--- components/main_page/about_us.php
+++ components/main_page/about_us.php
@@ -1,17 +1,15 @@
 <section id="about_us">
-    <div class="container article-text-sg">
-        <div class="d-flex flex-row article-text-sg__content">
-            <div class="d-flex w-50 me-4 flex-column left-column">
-                <h3><span>EasyGifts - </span>gadżety to nie wszystko!</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Vivamus dictum turpis id diam accumsan, ut rhoncus purusviverra. Donec velit elit, euismod eget diam quis, eleifendtincidunt arcu. Phasellus vitae elementum urna, a molestieerat.</p>
-                <p>Proin scelerisque lobortis vehicula. Ut non lacus ut diamrhoncus hendrerit a eget arcu. Donec elementum vestibulumdiam, at laoreet ipsum maximus ut. Morbi efficitur nunc atligula dapibus, feugiat ultrices lorem vulputate. Nullamgravida risus nibh, vitae convallis diam pulvinar eget. Nullamid tortor arcu.</p>
-            </div>
-            <div class="d-flex w-50 ms-4 flex-column right-column">
-                <h3>MISJA</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Vivamus dictum turpis id diam accumsan, ut rhoncus purusviverra. Donec velit elit, euismod eget diam quis, eleifendtincidunt arcu.</p>
-                <h3>WIZJA</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Vivamus dictum turpis id diam accumsan, ut rhoncus purusviverra. Donec velit elit, euismod eget diam quis, eleifendtincidunt arcu.</p>
-            </div>
+    <div class="container about-us-box">
+        <div class="about-us-box__img">
+            <img src="./assets/img/main_page/about_us/bulb.jpg" alt="obraz" width="378" height="408">
+        </div>
+        <div class="about-us-box__text">
+            <h2>Dlaczego my?</h2>
+            <h3>Gadżety reklamowe - to proste!</h3>
+            <p><strong>Oferujemy najciekawszy wybór gadżetów reklamowych oraz darmowe narzędzia  wspierające sprzedaż dla agencji reklamowych 
+            - sprawdź co możesz zyskać!</strong></p>
+            <p>Jesteś przedstawicielem agencji reklamowej? Szukasz dostawcy gadżetów  reklamowych, który nie tylko zrealizuje Twoje zamówienia kompleksowo,  ale też zapewni wsparcie w codziennej pracy?</p>
+            <p>Poznaj nas! Jesteśmy jednym z największych importerów gadżetów promocyjnych w Polsce. <strong>Współpracujemy tylko i wyłącznie z agencjami reklamowymi i pośrednikami dzięki czemu  możemy zapewnić najwyższą jakość usług na całym etapie realizacji  zlecenia.</strong> Własna drukarnia i zespół specjalistów pozwalają nam  zapewnić ekspresowe wykonanie znakowania na gadżetach. Mamy kreatywne  podejście do nadruków i właśnie tym podejściem pragniemy dzielić się z  naszymi Klientami.</p>
         </div>
     </div>
 </section>
