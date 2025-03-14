3b95b52b46816ae105f6145248c23a49093b0b80
goldipl <mgodlewski92@gmail.com>
2025-03-14 21:55:21 +0100

    refactor breadcrumbs styles; update hover effects and remove unused doming page

--- grawer.php
+++ grawer.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>EasyGifts - Doming</title>
+        <title>EasyGifts - Grawer laserowy</title>
         <link rel="shortcut icon" href="./assets/icons/favicon.ico" type="image/x-icon">
         <link rel="stylesheet" href="./css/bootstrap.min.css" crossorigin="anonymous">
         <link rel="stylesheet" href="./css/swiper-bundle.css" />
@@ -26,7 +26,7 @@
                         <a href="#">Znakowanie</a>
                     </li>
                     <li class="breadcrumb-item">
-                        <a href="#">Doming</a>
+                        <span>Grawer laserowy</span>
                     </li>
                 </ul>
             </div>
