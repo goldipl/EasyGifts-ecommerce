0af285227e84755adb67d5a8f9d342a43c4e8f6e
goldipl <mgodlewski92@gmail.com>
2025-03-14 16:43:36 +0100

    fill and styling footer

--- components/common/footer.php
+++ components/common/footer.php
@@ -1,6 +1,6 @@
 <div class="footer__container container">
     <div class="footer__container__top d-flex flex-row justify-content-between">
-        <div class="d-flex flex-column col-3 first-column">
+        <div class="d-flex flex-column col-4 first-column">
             <div class="footer-logo">
                 <img src="./assets/icons/common/footer/logo-easygifts-footer.svg" alt="EasyGifts - Dystrybutor gadżetów reklamowych" width="160" height="62">
             </div>
@@ -8,78 +8,76 @@
                 <strong>Tysiące gadżetów w jednych miejscu!</strong>
             </div>
             <div class="footer-text">
-                <p>Macma jest jednym z największych importerów i dostawców gadżetów reklamowych w Europie. Istniejemy już od 2003 roku, co pozwoliło nam na zdobycie doświadczenia i dużej znajomości branży reklamowej.</p>
+                <p>Oferujemy najciekawszy wybór gadżetów reklamowych oraz darmowe narzędzia  wspierające sprzedaż dla agencji reklamowych - sprawdź co możesz zyskać!</p>
             </div>
-            <div class="footer-text newsletter-title">
-                <strong>Zapisz się do newslettera</strong>
+            <div class="socials-footer">
+                <a href="" rel="nofollow" target="_blank">
+                    <img src="./assets/icons/common/footer/facebook.svg" alt="facebook" width="26" height="26">
+                </a>
+                <a href="" rel="nofollow" target="_blank">
+                    <img src="./assets/icons/common/footer/youtube.svg" alt="youtube" width="26" height="26">
+                </a>
+                <a href="" rel="nofollow" target="_blank">
+                    <img src="./assets/icons/common/footer/instagram.svg" alt="instagram" width="26" height="26">
+                </a>
+                <a href="" rel="nofollow" target="_blank">
+                    <img src="./assets/icons/common/footer/pinterest.svg" alt="pinterest" width="26" height="26">
+                </a>
+                <a href="" rel="nofollow" target="_blank">
+                    <img src="./assets/icons/common/footer/linkedin.svg" alt="linkedin" width="26" height="26">
+                </a>
             </div>
-            <form class="newsletter-form">
-                <div class="d-inline-flex">
-                    <input type="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" name="newsletter" id="newsletter_input" placeholder="Wpisz adres email" required>
-                    <button id="newsletter_btn">Wyślij</button>
-                </div>
-                <div class="d-flex mt-2 accepts-info">
-                    <label class="custom-checkbox-container">Akceptuję politykę prywatności
-                        <input type="checkbox" class="custom_input" required>
-                        <span class="custom-checkbox-checkmark"></span>
-                    </label>
-                </div>
-            </form>
         </div>
-        <div class="d-flex flex-column align-items-center col-3 second-column">
+        <div class="d-flex flex-column align-items-center col-2 second-column">
             <div class="footer-menu-box d-flex flex-column">
                 <h2>Menu</h2>
-                <a href="#">Oferta</a>
+                <a href="#">OFERTA NO1</a>
                 <a href="#">Marki</a>
+                <a href="#">Nowości</a>
                 <a href="#">Tematyczne</a>
                 <a href="#">Promocje</a>
-                <a href="#">Znakowanie</a>
-                <a href="#">Katalogi</a>
                 <a href="#">Import</a>
+                <a href="#">Katalogi</a>
+                <a href="#">Znakowanie</a>
+                <a href="#">Kontakt</a>
             </div>
         </div>
-        <div class="d-flex flex-column align-items-center col-3 third-column">
+        <div class="d-flex flex-column align-items-center col-2 third-column">
             <div class="footer-socials-box d-flex flex-column">
-                <h2>Social media</h2>
-                <a href="" rel="nofollow" target="_blank">
-                    <img src="./assets/icons/common/topbar/facebook.svg" alt="facebook" width="26" height="26">
-                </a>
-                <a href="" rel="nofollow" target="_blank">
-                    <img src="./assets/icons/common/topbar/youtube.svg" alt="youtube" width="26" height="26">
-                </a>
-                <a href="" rel="nofollow" target="_blank">
-                    <img src="./assets/icons/common/topbar/instagram.svg" alt="instagram" width="26" height="26">
-                </a>
-                <a href="" rel="nofollow" target="_blank">
-                    <img src="./assets/icons/common/topbar/pinterest.svg" alt="pinterest" width="26" height="26">
-                </a>
-                <a href="" rel="nofollow" target="_blank">
-                    <img src="./assets/icons/common/topbar/linkedin.svg" alt="linkedin" width="26" height="26">
-                </a>
+                <h2>Nasze marki</h2>
+                <a href="#">MoLu</a>
+                <a href="#">Victorinox</a>
+                <a href="#">Wenger</a>
+                <a href="#">Schwarzwolf</a>
+                <a href="#">Pierre Cardin</a>
+                <a href="#">Silicon Power</a>
             </div>
         </div>
-        <div class="d-flex flex-column align-items-center col-3 fourth-column">
+        <div class="d-flex flex-column align-items-center col-2 fourth-column">
+            <div class="footer-contacts-box d-flex flex-column">
+                <h2>Pomoc</h2>
+                <a href="#">Polityka prywatności </a>
+                <a href="#">Regulamin sklepu</a>
+                <a href="#">Najczęściej zadawane pytania</a>
+            </div>
+        </div>
+        <div class="d-flex flex-column align-items-center col-2 fifth-column">
             <div class="footer-contacts-box d-flex flex-column">
                 <h2>Kontakt</h2>
                 <div class="footer-contact-box">
-                    <strong>Biuro</strong>
-                    <p>tel. <a class="gtm-phone" href="tel:+48 71 330 54 60">+48 71 330 54 60</a></p>
-                    <p>e-mail: <a class="gtm-mail" href="mailto:biuro@macma.pl">biuro@macma.pl</a></p>
+                    <p><img src="./assets/icons/common/footer/contact.svg" alt="telefon" width="18" height="18"> <a class="gtm-phone" href="tel:+48 713 152 330">+48 713 152 330</a></p>
                 </div>
                 <div class="footer-contact-box">
-                    <strong>Drukarnia</strong>
-                    <p>tel. <a class="gtm-phone" href="tel:+48 71 397 87 00">+48 71 397 87 00</a></p>
-                    <p>e-mail: <a class="gtm-mail" href="mailto:drukarnia@macma.pl">drukarnia@macma.pl</a></p>
+                    <p><img src="./assets/icons/common/footer/letter.svg" alt="e-mail" width="18" height="18"> <a class="gtm-mail" href="mailto:biuro@easygifts.pl">biuro@easygifts.pl</a></p>
                 </div>
                 <div class="footer-contact-box">
-                    <strong>Magazyn</strong>
-                    <p>tel. <a class="gtm-phone" href="tel:+48 71 397 87 18">+48 71 397 87 18</a></p>
-                    <p>e-mail: <a class="gtm-mail" href="mailto:magazyn2@macma.pl">magazyn2@macma.pl</a></p>
+                    <p><img src="./assets/icons/common/footer/location.svg" alt="adres" width="18" height="18"></p>
+                    <div class="address-footer d-flex flex-column"><span>ul. Wrocławska 41 Byków</span> <span>55-095 Mirków</span></div>
                 </div>
             </div>
         </div>
     </div>
     <div class="footer__container__bottom d-flex justify-content-center align-items-center">
-        <p>© 2009 - <span></span> Macma Sp. z o. o.</p><p class="contip">Realizacja <a href="https://www.contip.net/"><img src="./assets/img/common/footer/contip-logo.png" height="24" alt="Contip logo"></a></p>
+        <p>All rights reserved. Copyright by PCM Sp. z o.o. EASY GIFTS Sp.k. 2016 - <span></span>.</p><p class="contip">Realizacja <a href="https://www.contip.net/"><img src="./assets/img/common/footer/contip-logo.png" height="24" alt="Contip logo"></a></p>
     </div>
 </div>
