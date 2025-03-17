dfe1bcd847abc77203c1764e4b4a4947cbafe24b
goldipl <mgodlewski92@gmail.com>
2025-03-17 18:39:32 +0100

    update catalogs content and styles for improved clarity and user experience

--- components/catalogs/catalogs_content.php
+++ components/catalogs/catalogs_content.php
@@ -1,16 +1,16 @@
 <div class="catalogs-container container">
-    <h1>Katalogi</h1>
-    <h4>Jesteśmy dystrybutorem gadżetów reklamowych</h4>
+    <h1>Katalogi Easy Gifts</h1>
+    <h4>Tutaj znajdziesz wszystkie aktualne materiały, które przygotowaliśmy, aby ułatwić Ci zapoznanie się z naszą ofertą. Niezależnie od tego, czy szukasz szczegółowych informacji o produktach, danych technicznych, inspiracji czy promocji – nasze katalogi są do Twojej dyspozycji.</h4>
     <div class="catalogs-wrapper">
         <div class="catalogs-wrapper_col">
             <div class="catalogs-wrapper_col_top">
                 <div class="catalogs-wrapper_col_top_left">
-                    <img src="./assets/img/catalogs/catalogs-00.jpg" alt="Katalog" width="248" height="349">
+                    <img src="./assets/img/catalogs/catalogs-01.jpg" alt="Katalog" width="248" height="349">
                 </div>
                 <div class="catalogs-wrapper_col_top_right">
                     <div class="catalogs-text">
-                        <h2>Macma News 2024</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur. Magna eget nunc tincidunt gravida eu scelerisque duis tincidunt. Lacinia sed a id non lorem. Urna ultricies nec sed sed volutpat. Enim a maecenas nulla sit ipsum sociis lorem aliquam. Consectetur nam mauris neque ipsum ultrices odio quis amet integer. Nunc faucibus molestie quis ac in. Urna et senectus amet velit in. Ut urna urna risus tellus vitae senectus facilisi vel. Id risus vitae lorem mi felis pretium ut ultricies. Facilisis dictum orci egestas at. Vitae integer diam ultricies leo rutrum urna. Vulputate etiam id tincidunt massa elit nulla. Commodo dignissim suspendisse etiam convallis parturient eget sed enim. Augue sem enim aliquam in justo venenatis gravida. Vitae ipsum quam feugiat integer vitae nulla quis sed tempor. Quis dictum et nunc ut enim donec. Mattis dolor sed dignissim quam feugiat erat sagittis.</p>
+                        <h2>Katalog No.1</h2>
+                        <p>Katalog NO.1 to niezastąpione narzędzie dla profesjonalistów  poszukujących inspiracji do innowacyjnych metod promocji marki. Zawiera  obszerny asortyment gadżetów z różnych segmentów. Znajdziesz w nim  produkty dostosowane do różnorodnych potrzeb - od zaawansowanej  elektroniki po eleganckie akcesoria biurowe oraz przedmioty codziennego  użytku.</p>
                     </div>
                 </div>
             </div>
@@ -39,7 +39,7 @@
                 <div class="catalogs-wrapper_col_top_right">
                     <div class="catalogs-text">
                         <h2>SUSTAINABLE PROMOTION PRODUCTS</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur. Magna eget nunc tincidunt gravida eu scelerisque duis tincidunt. Lacinia sed a id non lorem. Urna ultricies nec sed sed volutpat. Enim a maecenas nulla sit ipsum sociis lorem aliquam. Consectetur nam mauris neque ipsum ultrices odio quis amet integer. Nunc faucibus molestie quis ac in. Urna et senectus amet velit in. Ut urna urna risus tellus vitae senectus facilisi vel. Id risus vitae lorem mi felis pretium ut ultricies. Facilisis dictum orci egestas at. Vitae integer diam ultricies leo rutrum urna. Vulputate etiam id tincidunt massa elit nulla. Commodo dignissim suspendisse etiam convallis parturient eget sed enim. Augue sem enim aliquam in justo venenatis gravida. Vitae ipsum quam feugiat integer vitae nulla quis sed tempor. Quis dictum et nunc ut enim donec. Mattis dolor sed dignissim quam feugiat erat sagittis.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur. Magna eget nunc tincidunt gravida eu scelerisque duis tincidunt. Lacinia sed a id non lorem. Urna ultricies nec sed sed volutpat. Enim a maecenas nulla sit ipsum sociis lorem aliquam. Consectetur nam mauris neque ipsum ultrices odio quis amet integer. Nunc faucibus molestie quis ac in. Urna et senectus amet velit in. Ut urna urna risus tellus vitae senectus facilisi vel. Id risus vitae lorem mi felis pretium ut ultricies. Facilisis dictum orci egestas at. Vitae integer diam ultricies leo rutrum urna. Vulputate etiam id tincidunt massa elit nulla. Commodo dignissim suspendisse etiam convallis parturient eget sed enim. Augue sem enim aliquam in justo venenatis gravida. Vitae ipsum quam feugiat integer vitae nulla quis sed tempor. Quis dictum et nunc ut enim donec.</p>
                     </div>
                 </div>
             </div>
