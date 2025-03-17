038d0b8451c998d4b9cc2ee3d00b2dfc9ff49e44
goldipl <mgodlewski92@gmail.com>
2025-03-17 19:23:31 +0100

    refactor category styles for improved layout, accessibility, and responsiveness

--- components/category/category_left_filters.php
+++ components/category/category_left_filters.php
@@ -6,7 +6,7 @@
     <div class="category-left-filters">
         <div class="category-left-filters__slot">
             <div class="header">
-                <h3>Sortuj</h3>
+                <h3>Filtruj</h3>
             </div>
             <div class="content">
                 <label class="custom-checkbox-container">Nowość <input type="checkbox" class="custom_input">
@@ -19,21 +19,6 @@
                     <span class="custom-checkbox-checkmark"></span>
                 </label>
                 <label class="custom-checkbox-container">Ekologiczny <input type="checkbox" class="custom_input">
-                    <span class="custom-checkbox-checkmark"></span>
-                </label>
-            </div>
-        </div>
-        <div class="category-left-filters__slot price-type">
-            <div class="header">
-                <h3>Typ ceny</h3>
-            </div>
-            <div class="content">
-                <label class="custom-checkbox-container">
-                    Katalogowa <input type="checkbox" class="custom_input" name="priceType" onclick="handleCheckboxClick(this)">
-                    <span class="custom-checkbox-checkmark"></span>
-                </label>
-                <label class="custom-checkbox-container">
-                    Twoja cena <input type="checkbox" class="custom_input" name="priceType" onclick="handleCheckboxClick(this)">
                     <span class="custom-checkbox-checkmark"></span>
                 </label>
             </div>
@@ -51,6 +36,10 @@
                     <div class="row-inputs__right">
                         <input class="form-input" type="text" name="price-right-input" id="price-right-input" placeholder="Do" min="0">
                     </div>
+                    <span class="spacer btn-spacer"></span>
+                    <div class="row-inputs__right">
+                        <button type="submit" class="color-btn small-btn">Zastosuj</button>                            
+                    </div>
                 </div>
             </div>
         </div>
@@ -59,7 +48,13 @@
                 <h3>Ilość</h3>
             </div>
             <div class="content">
-                <input class="form-input" type="text" name="quantity-input" id="quantity-input" min="1">
+                <div class="row-inputs">
+                    <input class="form-input" type="text" name="quantity-input" id="quantity-input" min="1">
+                    <span class="spacer btn-spacer"></span>
+                    <div class="row-inputs__right">
+                        <button type="submit" class="color-btn small-btn">Zastosuj</button>                            
+                    </div>
+                </div>
             </div>
         </div>
         <div class="category-left-filters__slot">
@@ -131,6 +126,28 @@
         </div>
         <div class="category-left-filters__slot">
             <div class="header">
+                <h3>Znakowanie</h3>
+            </div>
+            <div class="content">
+                <label class="custom-checkbox-container">Tampodruk <input type="checkbox" class="custom_input">
+                    <span class="custom-checkbox-checkmark"></span>
+                </label>
+                <label class="custom-checkbox-container">Druk UV <input type="checkbox" class="custom_input">
+                    <span class="custom-checkbox-checkmark"></span>
+                </label>
+                <label class="custom-checkbox-container">Grawer laserowy <input type="checkbox" class="custom_input">
+                    <span class="custom-checkbox-checkmark"></span>
+                </label>
+                <label class="custom-checkbox-container">Sublimacja <input type="checkbox" class="custom_input">
+                    <span class="custom-checkbox-checkmark"></span>
+                </label>
+                <label class="custom-checkbox-container">Doming <input type="checkbox" class="custom_input">
+                    <span class="custom-checkbox-checkmark"></span>
+                </label>
+            </div>
+        </div>
+        <div class="category-left-filters__slot">
+            <div class="header">
                 <h3>Marka</h3>
             </div>
             <div class="content">
@@ -184,66 +201,9 @@
                     <span class="custom-checkbox-checkmark"></span>
                 </label>
             </div>
-            <div class="show-more">
-                <span>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" viewBox="0 0 11 11" fill="none">
-                        <path d="M5 1V10.5" stroke="#374957" stroke-linecap="round" stroke-linejoin="round" />
-                        <path d="M10 5.75L0.5 5.75" stroke="#374957" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>Więcej 
-                </span>
-            </div>
-            <div class="content hidden">
-                <label class="custom-checkbox-container">Ukryty filtr <input type="checkbox" class="custom_input">
-                    <span class="custom-checkbox-checkmark"></span>
-                </label>
-                <label class="custom-checkbox-container">Ukryty filtr <input type="checkbox" class="custom_input">
-                    <span class="custom-checkbox-checkmark"></span>
-                </label>
-                <label class="custom-checkbox-container">Ukryty filtr <input type="checkbox" class="custom_input">
-                    <span class="custom-checkbox-checkmark"></span>
-                </label>
-            </div>
         </div>
-        <div class="category-left-filters__slot">
-            <div class="header">
-                <h3>Technika znakowania</h3>
-            </div>
-            <div class="content">
-                <label class="custom-checkbox-container">Tampodruk <input type="checkbox" class="custom_input">
-                    <span class="custom-checkbox-checkmark"></span>
-                </label>
-                <label class="custom-checkbox-container">Druk UV <input type="checkbox" class="custom_input">
-                    <span class="custom-checkbox-checkmark"></span>
-                </label>
-                <label class="custom-checkbox-container">Grawer laserowy <input type="checkbox" class="custom_input">
-                    <span class="custom-checkbox-checkmark"></span>
-                </label>
-                <label class="custom-checkbox-container">Sublimacja <input type="checkbox" class="custom_input">
-                    <span class="custom-checkbox-checkmark"></span>
-                </label>
-                <label class="custom-checkbox-container">Doming <input type="checkbox" class="custom_input">
-                    <span class="custom-checkbox-checkmark"></span>
-                </label>
-            </div>
-            <div class="show-more">
-                <span>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" viewBox="0 0 11 11" fill="none">
-                        <path d="M5 1V10.5" stroke="#374957" stroke-linecap="round" stroke-linejoin="round" />
-                        <path d="M10 5.75L0.5 5.75" stroke="#374957" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>Więcej 
-                </span>
-            </div>
-            <div class="content hidden">
-                <label class="custom-checkbox-container">Ukryty filtr <input type="checkbox" class="custom_input">
-                    <span class="custom-checkbox-checkmark"></span>
-                </label>
-                <label class="custom-checkbox-container">Ukryty filtr <input type="checkbox" class="custom_input">
-                    <span class="custom-checkbox-checkmark"></span>
-                </label>
-                <label class="custom-checkbox-container">Ukryty filtr <input type="checkbox" class="custom_input">
-                    <span class="custom-checkbox-checkmark"></span>
-                </label>
-            </div>
+        <div class="category-left-filters__slot buttons">
+            <a href="" class="white-btn filter-clear">Wyczyść</a>                
         </div>
     </div>
 </div>
