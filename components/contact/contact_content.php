a2face01b67de6cfe039380a3858c57cc7fd6b0f
goldipl <mgodlewski92@gmail.com>
2025-03-17 18:18:33 +0100

    refactor contact sales department styles and update other departments' contact information for improved layout and accessibility

--- components/contact/contact_content.php
+++ components/contact/contact_content.php
@@ -8,17 +8,24 @@
             </div>
             <div class="contact-wrapper_col__content">
                 <div class="data-box location">
-                    <b>Macma Polska Sp. z o.o.</b>
-                    <p>Byków, ul. Wrocławska 41</p>
-                    <p>55-095 Mirków k/Wrocławia</p>
+                    <strong>PCM Sp. z o.o. EASY GIFTS Sp.k.</strong>
+                    <p>ul. Wrocławska 41 Byków</p>
+                    <p>55-095 Mirków</p>
+                </div>
+                <div class="data-box phone">
+                    <p class="phone"><a class="gtm-phone" href="tel:+48 71 315 23 30">+48 71 315 23 30</a></p>
+                </div>
+                <div class="data-box mail">
+                    <p class="mail"><a class="gtm-mail" href="mailto:biuro@easygifts.pl">biuro@easygifts.pl</a></p>
+                </div>
+                <div class="data-box hours">
+                    <p>czynne pon. - pt.: 08:00 - 16:00</p>
                 </div>
                 <div class="data-box data">
-                    <p>NIP: PL8951782271</p>
-                    <p>REGON 932846103</p>
-                    <p>Sąd Rejonowy dla Wrocławia - Fabrycznej we</p>
-                    <p>Wrocławiu, VI Wydział Gospodarczy </p>
-                    <p>KRS 0000144153</p>
-                    <p>Kapitał zakładowy (wpłacony) 850 000 PLN</p>
+                    <p>NIP: PL8961447858</p>
+                    <p>REGON: 020635065</p>
+                    <p>KRS: 0000457282</p>
+                    <p>GIOŚ: E0021035WZBW</p>
                 </div>
             </div>
             <div class="contact-wrapper_col__content map">
@@ -27,28 +34,23 @@
         </div>
         <div class="contact-wrapper_col">
             <div class="title">
-                <p>Kontakt</p>
+                <p>Działy</p>
             </div>
             <div class="contact-wrapper_col__content">
                 <div class="contact-box">
-                    <h3 class="office">Biuro</h3>
-                    <p class="phone"><a class="gtm-phone" href="tel:+48 71 330 54 60">+48 71 330 54 60</a></p>
-                    <p class="mail"><a class="gtm-mail" href="mailto:biuro@macma.pl">biuro@macma.pl</a></p>
-                    <p class="hours">Biuro czynne: poniedziałek-piątek: 8:00-16:00</p>
-                </div>
-                <div class="horizontal-line"></div>
-                <div class="contact-box">
-                    <h3 class="print">Drukarnia</h3>
-                    <p class="phone"><a class="gtm-phone" href="tel:+48 71 397 87 00">+48 71 397 87 00</a></p>
-                    <p class="mail"><a class="gtm-mail" href="mailto:drukarnia@macma.pl">drukarnia@macma.pl</a></p>
-                    <p class="hours">Drukarnia czynna: poniedziałek-piątek: 6:00-14:00</p>
-                </div>
-                <div class="horizontal-line"></div>
-                <div class="contact-box">
                     <h3 class="storage">Magazyn</h3>
-                    <p class="phone"><a class="gtm-phone" href="tel:+48 71 397 87 18">+48 71 397 87 18</a></p>
-                    <p class="mail"><a class="gtm-mail" href="mailto:magazyn2@macma.pl">magazyn2@macma.pl</a></p>
-                    <p class="hours">Magazyn czynny: poniedziałek-piątek: 8:00-16:00</p>
+                    <p class="phone"><a class="gtm-phone" href="tel:+48 71 397 87 16">+48 71 397 87 16</a></p>
+                    <p class="phone2"><a class="gtm-phone" href="tel:+48 668 599 127">+48 668 599 127</a></p>
+                    <p class="mail"><a class="gtm-mail" href="mailto:magazyn@easygifts.pl">magazyn@easygifts.pl</a></p>
+                    <p class="hours">czynne pon. - pt.: 08:00 - 16:00</p>
+                </div>
+                <div class="horizontal-line"></div>
+                <div class="contact-box">
+                    <h3 class="storage">Drukarnia</h3>
+                    <p class="phone"><a class="gtm-phone" href="tel: +48 71 397 87 04"> +48 71 397 87 04</a></p>
+                    <p class="phone2"><a class="gtm-phone" href="tel:+48 600 375 241">+48 600 375 241</a></p>
+                    <p class="mail"><a class="gtm-mail" href="mailto:drukarnia@easygifts.pl">drukarnia@easygifts.pl</a></p>
+                    <p class="hours">czynne: pon. - pt.: 6:00 - 14:00</p>
                 </div>
             </div>
         </div>
@@ -60,27 +62,27 @@
                 <form class="send-form">
                     <div class="input-slot">
                         <label for="name-surname">Imię i nazwisko</label>
-                        <input class="form-input" type="text" name="name-surname" id="name-surname" required>
+                        <input class="form-input" type="text" name="name-surname" id="name-surname" placeholder="Jan Nowak" required>
                     </div>
                     <div class="input-slot">
                         <label for="company">Firma</label>
-                        <input class="form-input" type="text" name="company" id="company">
+                        <input class="form-input" type="text" name="company" id="company" placeholder="Firma1">
                     </div>
                     <div class="input-slot">
                         <label for="nip-code">NIP</label>
-                        <input class="form-input" type="text" name="nip-code" id="nip-code">
+                        <input class="form-input" type="text" name="nip-code" id="nip-code" placeholder="54111474">
                     </div>
                     <div class="input-slot">
                         <label for="email">Email</label>
-                        <input class="form-input" type="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" name="email" id="email">
+                        <input class="form-input" type="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" name="email" id="email" placeholder="mail@gmail.com" required>
                     </div>
                     <div class="input-slot">
                         <label for="telephone">Telefon</label>
-                        <input class="form-input" type="text" name="telephone" id="telephone" required>
+                        <input class="form-input" type="text" name="telephone" id="telephone" placeholder="555 555 666">
                     </div>
                     <div class="input-slot">
                         <label for="msgarea">Wiadomość</label>
-                        <textarea class="form-textarea" name="msgarea" id="msgarea" required></textarea>
+                        <textarea class="form-textarea" name="msgarea" id="msgarea" placeholder="Wiadomość...." required></textarea>
                     </div>
                     <div class="checkbox-button-box">
                         <label class="custom-checkbox-container">Administratorem Państwa danych osobowych jest PCM Sp. z o.o. EASY GIFTS Sp.k. (NIP: 8961447858). Dane osobowe przetwarzane będą wyłącznie w prawnie usprawiedliwionych celach administratora danych polegających na udzieleniu odpowiedzi na Państwa wiadomość. Podanie przez Państwa danych osobowych jest dobrowolne, ale też niezbędne do nawiązania z Państwem kontaktu za pomocą formularza kontaktowego. Szczegółowe informacje na temat przetwarzania Państwa danych osobowych mogą Państwo znaleźć w naszej <a href="#">Polityce prywatności</a> na temat zasad przetwarzania danych osobowych.<input type="checkbox" class="custom_input"><span class="custom-checkbox-checkmark"></span>
--- components/contact/contact_other_departments_content.php
+++ components/contact/contact_other_departments_content.php
@@ -1,31 +1,75 @@
 <div class="other-department-box container">
-    <div class="other-department-box__title">
-        <p>Pozostałe działy</p>
+  <div class="other-department-box__title">
+    <p>Pozostałe działy</p>
+  </div>
+  <div class="other-department-box__row">
+    <div class="other-department-box__row__col">
+      <div class="department-top-box">
+        <b class="marketing-department">Marketing</b>
+      </div>
+      <div class="horizontal-line"></div>
+      <div class="department-bottom-box">
+        <p class="phone">
+          <a class="gtm-phone" href="tel:+48 71 330 51 100">+48 71 330 51 100</a>
+        </p>
+        <p class="phone">
+          <a class="gtm-phone" href="tel:+ 48 71 330 51 43">+ 48 71 330 51 43</a>
+        </p>
+        <p class="mail">
+          <a class="gtm-mail" href="mailto:marketing@easygifts.pl">marketing@easygifts.pl</a>
+        </p>
+      </div>
     </div>
-    <div class="other-department-box__row">
-        <div class="other-department-box__row__col">
-            <b class="graphic-department">Dział Graficzny</b>
-            <p class="phone"><a class="gtm-phone" href="tel:+48 71 330 54 60">+48 71 330 54 60</a></p>
-            <p class="mail"><a class="gtm-mail" href="mailto:biuro@macma.pl">biuro@macma.pl</a></p>
-            <p class="hours">Biuro czynne: pon. - pt. 8:00 – 16:00</p>
-        </div>
-        <div class="other-department-box__row__col">
-            <b class="accountancy">Księgowość</b>
-            <p class="phone"><a class="gtm-phone" href="tel:+48 71 330 54 60">+48 71 330 54 60</a></p>
-            <p class="mail"><a class="gtm-mail" href="mailto:biuro@macma.pl">biuro@macma.pl</a></p>
-            <p class="hours">Biuro czynne: pon. - pt. 8:00 – 16:00</p>
-        </div>
-        <div class="other-department-box__row__col">
-            <b class="marketing-department">Dział Marketingu</b>
-            <p class="phone"><a class="gtm-phone" href="tel:+48 71 330 54 60">+48 71 330 54 60</a></p>
-            <p class="mail"><a class="gtm-mail" href="mailto:biuro@macma.pl">biuro@macma.pl</a></p>
-            <p class="hours">Biuro czynne: pon. - pt. 8:00 – 16:00</p>
-        </div>
-        <div class="other-department-box__row__col">
-            <b class="import-department">Dział Importu</b>
-            <p class="phone"><a class="gtm-phone" href="tel:+48 71 330 54 60">+48 71 330 54 60</a></p>
-            <p class="mail"><a class="gtm-mail" href="mailto:biuro@macma.pl">biuro@macma.pl</a></p>
-            <p class="hours">Biuro czynne: pon. - pt. 8:00 – 16:00</p>
-        </div>
+    <div class="other-department-box__row__col">
+      <div class="department-top-box">
+        <b class="import-department">Import</b>
+      </div>
+      <div class="horizontal-line"></div>
+      <div class="department-bottom-box">
+        <p class="phone">
+          <a class="gtm-phone" href="tel:+48 71 397 87 27">+48 71 397 87 27</a>
+        </p>
+        <p class="phone2">
+          <a class="gtm-phone" href="tel:+48 602 365 366">+48 602 365 366</a>
+        </p>
+        <p class="mail">
+          <a class="gtm-mail" href="mailto:import@easygifts.pl">import@easygifts.pl</a>
+        </p>
+      </div>
     </div>
+    <div class="other-department-box__row__col">
+      <div class="department-top-box">
+        <b class="tax-department">Księgowość</b>
+      </div>
+      <div class="horizontal-line"></div>
+      <div class="department-bottom-box">
+        <p class="phone">
+          <a class="gtm-phone" href="tel:+48 71 330 54 76">+48 71 330 54 76</a>
+        </p>
+        <p class="phone2">
+          <a class="gtm-phone" href="tel:+48 604 961 800">+48 604 961 800</a>
+        </p>
+        <p class="mail">
+          <a class="gtm-mail" href="mailto:e.kroner@easygifts.pl">e.kroner@easygifts.pl</a>
+        </p>
+      </div>
+    </div>
+    <div class="other-department-box__row__col">
+      <div class="department-top-box">
+        <b class="graphic-department">Graficy</b>
+      </div>
+      <div class="horizontal-line"></div>
+      <div class="department-bottom-box">
+        <p class="phone">
+          <a class="gtm-phone" href="tel:+48 71 397 87 32">+48 71 397 87 32</a>
+        </p>
+        <p class="phone2">
+          <a class="gtm-phone" href="tel:+48 787 312 848">+48 787 312 848</a>
+        </p>
+        <p class="mail">
+          <a class="gtm-mail" href="mailto:dtp@easygifts.pl">dtp@easygifts.pl</a>
+        </p>
+      </div>
+    </div>
+  </div>
 </div>
