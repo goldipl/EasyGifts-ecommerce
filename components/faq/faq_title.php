671245cdade2ea4a091416b853b88f15987538fd
goldipl <mgodlewski92@gmail.com>
2025-03-18 21:07:32 +0100

    simplify FAQ section title markup for cleaner layout

--- components/faq/faq_title.php
+++ components/faq/faq_title.php
@@ -1,15 +1,5 @@
 <section id="top_title" class="news_title">
-    <div class="container d-flex flex-row align-items-center justify-content-center">
-        <div class="d-flex flex-column py-4 mp-titles align-items-center justify-content-center">
-            <div class="d-flex flex-row">
-            </div>
-            <div class="d-flex flex-row mp-title">
-                <h1 class="p-0">
-                    <strong>FAQ</strong>
-                </h1>
-            </div>
-        </div>
-        <div class="d-flex flex-row mp-bell-btn-box">
-        </div>
+    <div class="container">
+        <h1>FAQ</h1>
     </div>
 </section>
