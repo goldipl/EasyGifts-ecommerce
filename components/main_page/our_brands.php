c60912564718be2713b87ffad89d27fbc7c6b081
goldipl <mgodlewski92@gmail.com>
2025-03-25 17:53:38 +0100

    remove outdated brand images and update to new ones for improved representation

--- components/main_page/our_brands.php
+++ components/main_page/our_brands.php
@@ -21,73 +21,37 @@
                 <!-- Slide 01 -->
                 <div class="swiper-slide">
                     <a href="#" class="d-flex flex-column align-items-center justify-content-center text-decoration-none">
-                        <img src="./assets/img/main_page/our_brands/Schwarzwolf.jpg" alt="brand img" width="222" height="111">
+                        <img src="./assets/img/main_page/our_brands/pierre_cardin.jpg" alt="brand img" width="222" height="111">
                     </a>
                 </div>
                 <!-- Slide 02 -->
                 <div class="swiper-slide">
                     <a href="#" class="d-flex flex-column align-items-center justify-content-center text-decoration-none">
-                        <img src="./assets/img/main_page/our_brands/M-collections.jpg" alt="brand img" width="222" height="111">
-                    </a>
-                </div>
-                <!-- Slide 03 -->
-                <div class="swiper-slide">
-                    <a href="#" class="d-flex flex-column align-items-center justify-content-center text-decoration-none">
-                        <img src="./assets/img/main_page/our_brands/ferraghini.jpg" alt="brand img" width="222" height="111">
-                    </a>
-                </div>
-                <!-- Slide 04 -->
-                <div class="swiper-slide">
-                    <a href="#" class="d-flex flex-column align-items-center justify-content-center text-decoration-none">
-                        <img src="./assets/img/main_page/our_brands/crisma.jpg" alt="brand img" width="222" height="111">
-                    </a>
-                </div>
-                <!-- Slide 05 -->
-                <div class="swiper-slide">
-                    <a href="#" class="d-flex flex-column align-items-center justify-content-center text-decoration-none">
-                        <img src="./assets/img/main_page/our_brands/mark_twain.jpg" alt="brand img" width="222" height="111">
-                    </a>
-                </div>
-                <!-- Slide 06 -->
-                <div class="swiper-slide">
-                    <a href="#" class="d-flex flex-column align-items-center justify-content-center text-decoration-none">
-                        <img src="./assets/img/main_page/our_brands/pierre_cardin.jpg" alt="brand img" width="222" height="111">
-                    </a>
-                </div>
-                <!-- Slide 01 -->
-                <div class="swiper-slide">
-                    <a href="#" class="d-flex flex-column align-items-center justify-content-center text-decoration-none">
                         <img src="./assets/img/main_page/our_brands/Schwarzwolf.jpg" alt="brand img" width="222" height="111">
                     </a>
                 </div>
-                <!-- Slide 02 -->
-                <div class="swiper-slide">
-                    <a href="#" class="d-flex flex-column align-items-center justify-content-center text-decoration-none">
-                        <img src="./assets/img/main_page/our_brands/M-collections.jpg" alt="brand img" width="222" height="111">
-                    </a>
-                </div>
                 <!-- Slide 03 -->
                 <div class="swiper-slide">
                     <a href="#" class="d-flex flex-column align-items-center justify-content-center text-decoration-none">
-                        <img src="./assets/img/main_page/our_brands/ferraghini.jpg" alt="brand img" width="222" height="111">
+                        <img src="./assets/img/main_page/our_brands/wenger.png" alt="brand img" width="222" height="111">
                     </a>
                 </div>
                 <!-- Slide 04 -->
                 <div class="swiper-slide">
                     <a href="#" class="d-flex flex-column align-items-center justify-content-center text-decoration-none">
-                        <img src="./assets/img/main_page/our_brands/crisma.jpg" alt="brand img" width="222" height="111">
+                        <img src="./assets/img/main_page/our_brands/victorinox.png" alt="brand img" width="222" height="111">
                     </a>
                 </div>
                 <!-- Slide 05 -->
                 <div class="swiper-slide">
                     <a href="#" class="d-flex flex-column align-items-center justify-content-center text-decoration-none">
-                        <img src="./assets/img/main_page/our_brands/mark_twain.jpg" alt="brand img" width="222" height="111">
+                        <img src="./assets/img/main_page/our_brands/slilicon-power.png" alt="brand img" width="222" height="111">
                     </a>
                 </div>
                 <!-- Slide 06 -->
                 <div class="swiper-slide">
                     <a href="#" class="d-flex flex-column align-items-center justify-content-center text-decoration-none">
-                        <img src="./assets/img/main_page/our_brands/pierre_cardin.jpg" alt="brand img" width="222" height="111">
+                        <img src="./assets/img/main_page/our_brands/molu.png" alt="brand img" width="222" height="111">
                     </a>
                 </div>
             </div>
