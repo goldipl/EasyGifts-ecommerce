221f9e37917cb48953648c1d854dc4c262259551
goldipl <mgodlewski92@gmail.com>
2025-03-29 19:59:18 +0100

    remove unnecessary script for active menu link in instructions and videos pages for cleaner code

--- videos.php
+++ videos.php
@@ -31,9 +31,6 @@
         <footer> 
             <?php include "./components/common/footer.php"; ?> 
         </footer>
-        <script>
-            document.querySelector('.menu-links-box.big-to-download > a:nth-of-type(5)').classList.add('active');
-        </script>
         <script src="./js/jquery.min.js"></script>
         <script src="./js/popper.min.js"></script>
         <script src="./js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
