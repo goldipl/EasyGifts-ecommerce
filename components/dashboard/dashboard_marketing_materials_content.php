dbc15fcf87dcca55f82a9306836f5cc6570979f0
goldipl <mgodlewski92@gmail.com>
2025-03-29 19:48:05 +0100

    add preview icon to marketing materials section for enhanced user interaction

--- components/dashboard/dashboard_marketing_materials_content.php
+++ components/dashboard/dashboard_marketing_materials_content.php
@@ -10,6 +10,9 @@
             <div class="product-content">
                 <p>Logo</p>
                 <div class="product-icons">
+                    <a class="preview me-2" target="_blank" href="">
+                        <img src="./assets/icons/dashboard/eye.svg" alt="menu icon" width="27" height="27">
+                    </a>
                     <a class="download" href="#">
                         <img src="./assets/icons/dashboard/download.svg" alt="menu icon" width="23" height="17">
                     </a>
