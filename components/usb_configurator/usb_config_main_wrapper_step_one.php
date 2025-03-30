1d4766eff052b2a2b1f820b778caa718069788df
goldipl <mgodlewski92@gmail.com>
2025-03-30 12:13:44 +0200

    enhance USB configurator layout and styles with responsive adjustments and additional summary details

--- components/usb_configurator/usb_config_main_wrapper_step_one.php
+++ components/usb_configurator/usb_config_main_wrapper_step_one.php
@@ -1,6 +1,6 @@
 <div class="usb_container">
     <div class="container">
-        <div class="d-flex flex-row">
+        <div class="d-flex flex-row usb-row-grid">
             <div class="accordion-container d-flex flex-column">
                 <div class="accordion me-2" id="accordionExample">
                     <div class="accordion-item" id="step-one">
@@ -294,14 +294,40 @@
                         <p>Podsumowanie</p>
                     </div>
                     <div class="usb-summary-box__usb-quantity">
+                        <p class="subtitle">Ilość</p>
+                        <p class="quantity">100 szt.</p>
                     </div>
                     <div class="usb-summary-box__usb-price">
+                        <p class="subtitle">Wycena: <span class="blue"> Rabat 1%</span></p>
+                        <small>Cena jednostkowa: <span class="green">14,09 PLN</span><s class="red">14,23 PLN</s></small>
+                        <small>Łączna wartość netto: <span class="green">1409,00 PLN</span><s class="red">1423,00 PLN</s></small>
                     </div>
                     <div class="usb-summary-box__usb-availability">
+                        <p class="subtitle">Dostępność:</p>
+                        <small>100 szt. 2-3 dni (Znakowanie: dodatkowe - 2-3 dni)</small>
                     </div>
                     <div class="usb-summary-box__usb-branding">
+                        <p class="subtitle">Znakowanie:</p>
+                        <small>T1USB2 (tampodruk USB 1 miejsce 2 kolory)</small>
                     </div>
                     <div class="usb-summary-box__usb-comments">
+                        <div class="usb-comments-box">
+                            <div class="usb-comments-box__img">
+                                <svg width="38" height="38" viewBox="0 0 38 38" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M19 0.25C8.65 0.25 0.25 8.65 0.25 19C0.25 29.35 8.65 37.75 19 37.75C29.35 37.75 37.75 29.35 37.75 19C37.75 8.65 29.35 0.25 19 0.25ZM19 30.25C17.625 30.25 16.5 29.125 16.5 27.75C16.5 26.375 17.625 25.25 19 25.25C20.375 25.25 21.5 26.375 21.5 27.75C21.5 29.125 20.375 30.25 19 30.25ZM21.8625 10.8625L21.025 20.8875C20.9375 21.9375 20.0625 22.75 19 22.75C17.9375 22.75 17.0625 21.9375 16.975 20.8875L16.1375 10.8625C16 9.1875 17.3125 7.75 19 7.75C20.6 7.75 21.875 9.05 21.875 10.625C21.875 10.7 21.875 10.7875 21.8625 10.8625Z" fill="#FF6174"/>
+                                </svg>
+                            </div>
+                            <div class="usb-comments-box__text">
+                                <p><strong>Uwaga: </strong> Zmień następujący element konfiguracji, aby uzyskać całość w ciągu 2-3 dni:</p>
+                                <p>Pamięć 3,0 (1000216) na:</p>
+                                <p>-1000264 (pojemność: 3.0 - 64GB,  szt.)</p>
+                                <p>-1000232 (pojemność: 3.0 - 32GB,  szt.)</p>
+                                <p>-1000164 (pojemność: 64GB,  szt.)</p>
+                                <p>-1000132 (pojemność: 32GB,  szt.)</p>
+                                <p>-1000116 (pojemność: 16GB,  szt.)</p>
+                                <p>-1000108 (pojemność: 8GB,  szt.).</p>
+                            </div>
+                        </div>
                     </div>
                     <div class="usb-summary-box__button">
                         <a href="#" class="color-btn next">Dalej <img src="./assets/icons/common/buttons/button_white_arrow.svg" alt="arrow icon" width="18" height="18"></a>
