b194ed0f2915c7ed3433ece478a89b50f55cce92
goldipl <mgodlewski92@gmail.com>
2025-03-31 19:46:38 +0200

    add responsive iframe support and improve gallery overflow handling

--- components/dashboard/dashboard_video_content.php
+++ components/dashboard/dashboard_video_content.php
@@ -4,6 +4,7 @@
         <div class="catalogs-wrapper_col video-slot">
             <div class="catalogs-wrapper_col_top">
                 <div class="catalogs-wrapper_col_top_right">
+                    <iframe class="float-start me-3" width="300" height="165" src="https://www.youtube.com/embed/G2EXsS0yu-0?si=ZR38iNQbsV65aL07" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                     <div class="catalogs-text">
                         <h2>Video No.1</h2>
                         <p>Szczelna, nie zawierająca BPA, sportowa butelka do picia wykonana z tritanu, z zamykaną pokrywką, paskiem na rękę.</p>
@@ -26,6 +27,7 @@
         <div class="catalogs-wrapper_col video-slot">
             <div class="catalogs-wrapper_col_top">
                 <div class="catalogs-wrapper_col_top_right">
+                <iframe class="float-start me-3" width="300" height="165" src="https://www.youtube.com/embed/G2EXsS0yu-0?si=ZR38iNQbsV65aL07" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                     <div class="catalogs-text">
                         <h2>Video No.1</h2>
                         <p>Szczelna, nie zawierająca BPA, sportowa butelka do picia wykonana z tritanu, z zamykaną pokrywką, paskiem na rękę.</p>
@@ -48,6 +50,7 @@
         <div class="catalogs-wrapper_col video-slot">
             <div class="catalogs-wrapper_col_top">
                 <div class="catalogs-wrapper_col_top_right">
+                <iframe class="float-start me-3" width="300" height="165" src="https://www.youtube.com/embed/G2EXsS0yu-0?si=ZR38iNQbsV65aL07" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                     <div class="catalogs-text">
                         <h2>Video No.1</h2>
                         <p>Szczelna, nie zawierająca BPA, sportowa butelka do picia wykonana z tritanu, z zamykaną pokrywką, paskiem na rękę.</p>
